fedff86568a33b3f486a1a878fbdf0d90dd4ad7c
rorensu2236 <rorensu2236@gmail.com>
2024-02-02 15:06:09 +0900

    default_exportを理解した

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function Profile() {
   );
 }
 
+// export default なので  import App from "./App"; で読み込め、Gallery が利用できるようになる（わかりずらい
+// export default でファイル名と全く違う動作をするコンポーネントを定義もできるがわかりづらくなるので、ファイル名と同じ名前にすることが多い
 export default function Gallery() {
   return (
     // <section>は小文字はじまりなのでhtmlタグとして認識される
