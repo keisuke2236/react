695cacfe66549b4454d4088f23db29b6469d6e74
rorensu2236 <rorensu2236@gmail.com>
2024-02-02 14:54:30 +0900

    default_exportと()について

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 function Profile() {
+  // return <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson" />;
+  // これなら()の中に書かなくてもいい
   return (
     <img
       src="https://i.imgur.com/MK3eW3As.jpg"
