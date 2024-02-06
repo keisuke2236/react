d93b7735b70973b01989cdd08557c004cadbc191
rorensu2236 <rorensu2236@gmail.com>
2024-02-06 20:08:42 +0900

    state構造の選択

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 // 章ごとにAppを切り替え
-import App from "./5-1/App";
+import App from "./5-3/App";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
